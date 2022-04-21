2c43f345e34a548b2bfb18447774ac0be5957bb0
Facite <89388117+facitea@users.noreply.github.com>
2022-04-21 16:05:26 +0900

    20220421 sev ver
    20220421 sev ver

--- data.js
+++ data.js
@@ -6,14 +6,13 @@ window.onload = function(){
     const year = today.getFullYear();
     const month = ('0' + (today.getMonth() + 1)).slice(-2);
     const day = ('0' + today.getDate()).slice(-2);
-    const dateString = year + '-' + month  + '-' + day; // 가입날짜와 오늘 날짜를 구분하면 됨
-    //let member_list_roof = member_list["member_info"][0]["m_regdate"];//각 콘텐츠를 dateString과 일치하는지 보기
+    const dateString = year + '-' + month  + '-' + day; // 오늘의 날짜 형식 조정 (yyyy-mm-dd->yyyy/mm/dd)
+    //각 콘텐츠를 dateString과 일치하는지 보기
     //a.includes(dateString); //0번째 문자열은 오늘날짜가 아니므로 false가 나옴
     //만약 true면 1을 더한다.    
 
     let member_list = JSON.parse(JSON.stringify(memberlist));
-    //let today_members = member_list["member_info"]["m_regdate"];
-    let total_members = member_list["member_info"].length;
+    let total_members = member_list["member_info"].length; //회원수
 
     let member_signup_date = new Array;
     for(let a=0; a<total_members; a++){
@@ -27,13 +26,14 @@ window.onload = function(){
                 resarr.push(member_signup_date[i]);
             }
         }
-    let today_total_signup = resarr.length; //최종 갯수
+    let today_total_signup = resarr.length; // 오늘의 가입자
     
-    console.log(resarr);
-    
+    let producer_list = JSON.parse(JSON.stringify(producerdata));
+    let total_producer = producer_list["producer_info"].length; //프로듀서 수
 
     document.getElementById("total_beat").innerText = "total";
     document.getElementById("today_beat").innerText = "today";
     document.getElementById("today_member").innerText = today_total_signup;
     document.getElementById("total_member").innerText = total_members;
+    document.getElementById("total_producer").innerText = total_producer;
 }
